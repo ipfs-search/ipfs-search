28f9f7a438071163dca67756ef48b924f293eadc
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-08-14 18:09:29 +0200

    Seperate crawler/seeder.

--- main.go
+++ main.go
@@ -20,9 +20,15 @@ func main() {
 
 	app.Commands = []cli.Command{
 		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "add `HASH` to crawler queue",
+			Action:  add,
+		},
+		{
 			Name:    "crawl",
 			Aliases: []string{"c"},
-			Usage:   "start crawling at `HASH`",
+			Usage:   "start crawler",
 			Action:  crawl,
 		},
 	}
@@ -64,15 +70,43 @@ func get_machinery() (*machinery.Server, error) {
 	return server, nil
 }
 
-func crawl(c *cli.Context) error {
+func add(c *cli.Context) error {
 	if c.NArg() != 1 {
 		return cli.NewExitError("Please supply one hash as argument.", 1)
 	}
 
-	start_hash := c.Args().Get(0)
+	hash := c.Args().Get(0)
 
-	fmt.Printf("Starting crawling with %s\n", start_hash)
+	fmt.Printf("Adding hash '%s' to queue\n", hash)
 
+	server, err := get_machinery()
+	if err != nil {
+		return cli.NewExitError(err.Error(), 1)
+	}
+
+	task := signatures.TaskSignature{
+		Name: "crawl",
+		Args: []signatures.TaskArg{
+			signatures.TaskArg{
+				Type:  "string",
+				Value: hash,
+			},
+		},
+	}
+	asyncResult, err := server.SendTask(&task)
+	if err != nil {
+		// failed to send the task
+		return cli.NewExitError(err.Error(), 1)
+	}
+
+	taskState := asyncResult.GetState()
+	fmt.Printf("Current state of %v task is:\n", taskState.TaskUUID)
+	fmt.Println(taskState.State)
+
+	return nil
+}
+
+func crawl(c *cli.Context) error {
 	// For now, assume gateway running on default host:port
 	sh := shell.NewShell("localhost:5001")
 
@@ -93,27 +127,7 @@ func crawl(c *cli.Context) error {
 		return crawli.CrawlHash(hash)
 	})
 
-	task := signatures.TaskSignature{
-		Name: "crawl",
-		Args: []signatures.TaskArg{
-			signatures.TaskArg{
-				Type:  "string",
-				Value: start_hash,
-			},
-		},
-	}
-
-	asyncResult, err := server.SendTask(&task)
-	if err != nil {
-		// failed to send the task
-		return cli.NewExitError(err.Error(), 1)
-	}
-
-	taskState := asyncResult.GetState()
-	fmt.Printf("Current state of %v task is:\n", taskState.TaskUUID)
-	fmt.Println(taskState.State)
-
-	worker := server.NewWorker("cli_worker")
+	worker := server.NewWorker("crawler")
 	err = worker.Launch()
 	if err != nil {
 		// do something with the error
