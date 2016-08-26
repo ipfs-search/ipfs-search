039b32724879122db581e18880ae26e4245e09e7
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-08-26 09:52:34 +0200

    Reduce timeouts to one minute for faster crawling.

--- main.go
+++ main.go
@@ -94,8 +94,8 @@ func crawl(c *cli.Context) error {
 	// For now, assume gateway running on default host:port
 	sh := shell.NewShell(IPFS_API)
 
-	// Set 2 minute timeout on IPFS requests
-	sh.SetTimeout(2 * time.Duration(time.Minute))
+	// Set 1 minute timeout on IPFS requests
+	sh.SetTimeout(1 * time.Duration(time.Minute))
 
 	el, err := get_elastic()
 	if err != nil {
