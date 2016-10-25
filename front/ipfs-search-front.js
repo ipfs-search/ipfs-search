4175d726b75e6f0451116e33a737ec888ecf7ce9
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-10-25 19:11:25 +0300

    Better looking results.

--- front/ipfs-search-front.js
+++ front/ipfs-search-front.js
@@ -50,6 +50,65 @@ function error_response(response, code, error) {
   response.end();
 }
 
+function get_title(result) {
+  // Get title from result
+
+  var src = result._source;
+  var titles = [];
+
+  // Try metadata
+  if ("metadata" in src) {
+    if (src.metadata.title) {
+      titles.push(src.metadata.title[0]);
+    }
+
+    if (src.metadata.name) {
+      titles.push(src.metadata.name[0]);
+    }
+  }
+
+  // // Try references (return the first)
+  src.references.forEach(function (item) {
+    if (item.name) {
+      titles.push(item.name);
+    }
+  });
+
+  // // Pick longest title
+  if (titles.length > 0) {
+    titles.sort(function (a, b) { return b.length - a.length });
+
+    return titles[0];
+  } else {
+    // Fallback to id
+    return result._id;
+  }
+}
+
+function transform_results(results) {
+  // Sanitize search results into a list like this:
+  // {
+  //   "hash": <>
+  //   "title": <>
+  //   "description": <>
+  // }
+  var hits = [];
+
+  results.hits.forEach(function (item) {
+    var title = get_title(item);
+    var description;
+
+    hits.push({
+      "hash": item._id,
+      "title": title,
+      "description": description
+    })
+  });
+
+  // Overwrite existing list of hits
+  results.hits = hits;
+}
+
 console.info("Starting server on http://localhost:"+port+"/");
 
 http.createServer(function(request, response) {
@@ -71,6 +130,8 @@ http.createServer(function(request, response) {
 
       query(parsed_url.query['q']).then(function (body) {
         console.info("200: Returning "+body.hits.hits.length+" results");
+
+        transform_results(body.hits);
 
         response.writeHead(200, {"Content-Type": "application/json"});
         response.write(JSON.stringify(body.hits));
