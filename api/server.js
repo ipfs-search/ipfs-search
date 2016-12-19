838cc99a9d65dea54ae1e9889e0dbd57d51ad123
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-12-19 10:52:24 +0100

    Correct ES port number.

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const htmlEncode = require('js-htmlencode');
 const server_port = 9615;
 
 var client = new elasticsearch.Client({
-  host: 'localhost:9201',
+  host: 'localhost:9200',
   log: 'info'
 });
 
