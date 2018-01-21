4b81a387766c3cd6f7c1681ce86150b8f7aafa14
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2018-01-21 14:16:16 +0000

    Revert "Close addChan in the right place."
    This reverts commit 52bd33c832cf46b81a5112b946dee837f2153774.

--- worker/worker.go
+++ worker/worker.go
@@ -24,11 +24,13 @@ func newCrawler(config *Config, addCh *queue.TaskChannel) (*crawler.Crawler, err
 	// As there's potential failure, execute this first to allow quick fail
 	hq, err := queue.NewTaskQueue(addCh, "hashes")
 	if err != nil {
+		addCh.Close()
 		return nil, err
 	}
 
 	fq, err := queue.NewTaskQueue(addCh, "files")
 	if err != nil {
+		addCh.Close()
 		return nil, err
 	}
 
@@ -38,6 +40,7 @@ func newCrawler(config *Config, addCh *queue.TaskChannel) (*crawler.Crawler, err
 
 	el, err := getElastic(config.ElasticSearchURL)
 	if err != nil {
+		addCh.Close()
 		return nil, err
 	}
 
@@ -67,7 +70,6 @@ func New(config *Config, errc chan<- error) (*Worker, error) {
 
 	c, err := newCrawler(config, addCh)
 	if err != nil {
-		addCh.Close()
 		return nil, err
 	}
 
