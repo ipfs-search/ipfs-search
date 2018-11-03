4997dd81b93621a95a1bad44586d25272d578bdb
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2018-11-03 22:09:38 +0000

    Upgrade IPFS, closes #68 and #62.

--- provisioning/roles/snifloop/files/bin/dht-sniffer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#ipfs log tail | jq -r 'if .event == "dhtSentMessage" and .message.type == "ADD_PROVIDER"  then .message.key else empty end'
-ipfs log tail | jq -r 'if .event == "handleAddProvider" then .key else empty end' | uniq
--- provisioning/roles/snifloop/files/bin/dht-snifloop-store.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true
-do
-	echo "Script stopped, starting again"
-	sudo -E -u ipfs ipfs log tail | jq -r 'if .event == "handleAddProvider" then .key else empty end' >> sniffed_hashes.txt	
-done
-
