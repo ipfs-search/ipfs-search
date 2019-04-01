9f6822853e89ade6e4a0013ba995da386004a019
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2019-04-01 23:51:20 +0100

    Stop snifloop from adding the same again and again
    Sorted uniqueness windowed per 1000 lines.

--- deployment/roles/snifloop/files/bin/dht-snifloop.sh
+++ deployment/roles/snifloop/files/bin/dht-snifloop.sh
@@ -3,6 +3,9 @@
 while true
 do
 	echo "Script stopped, starting again"
-	ipfs log tail | jq -r 'if .Operation == "handleAddProvider" then .Tags.key else empty end' | uniq | xargs -L 1 ipfs-search a
+	ipfs log tail | \
+        jq -r 'if .Operation == "handleAddProvider" then .Tags.key else empty end' |
+        sed '0~1000 s/$/\x00/g' | \
+        xargs -0 -I % bash -c "echo \"%\" | sort -u | xargs -L1 ipfs-search a"
 done
 
