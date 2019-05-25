7db0060551d1bf45c998d413042e71b00a28d61e
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2019-05-25 12:43:47 +0100

    buffer less keys before adding

--- deployment/roles/snifloop/files/bin/dht-snifloop.sh
+++ deployment/roles/snifloop/files/bin/dht-snifloop.sh
@@ -5,7 +5,7 @@ do
 	echo "Script stopped, starting again"
 	ipfs log tail |
         jq -r 'if .Operation == "handleAddProvider" then .Tags.key else empty end' |
-        sed '0~1000 s/$/\x00/g' |
+        sed '0~100 s/$/\x00/g' |
         xargs -0 -I % bash -c "echo \"%\" | sort -u | xargs -L1 ipfs-search a"
 done
 
