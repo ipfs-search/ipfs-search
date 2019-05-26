a9992e0390bb22919d0e480b4950e01395c093b4
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2019-05-26 21:41:36 +0100

    Ignore these ever recurring hashes.
    (It’s the README and something like that.)

--- deployment/roles/snifloop/files/bin/dht-snifloop.sh
+++ deployment/roles/snifloop/files/bin/dht-snifloop.sh
@@ -5,6 +5,8 @@ do
 	echo "Script stopped, starting again"
 	ipfs log tail |
         jq -r 'if .Operation == "handleAddProvider" then .Tags.key else empty end' |
+        grep -v QmY5heUM5qgRubMDD1og9fhCPA6QdkMp3QCwd4s7gJsyE7 |
+        grep -v QmSKboVigcD3AY4kLsob117KJcMHvMUu6vNFqk1PQzYUpp |
         sed '0~100 s/$/\x00/g' |
         xargs -0 -I % bash -c "echo \"%\" | sort -u | xargs -L1 ipfs-search a"
 done
