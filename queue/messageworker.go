830500802008c0afee0fe18c349690aeff276fae
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-05-21 16:44:04 +0100

    Don’t recover from panics! This is bad design!

--- queue/messageworker.go
+++ queue/messageworker.go
@@ -2,8 +2,6 @@ package queue
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"github.com/ipfs-search/ipfs-search/worker"
 	"github.com/streadway/amqp"
 	"log"
@@ -33,13 +31,6 @@ type messageWorker struct {
 
 // Work initiates the contained worker for a single message, acking if no error and rejecting otherwise
 func (m *messageWorker) Work(ctx context.Context) (err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			// Override original error value on panic
-			err = m.recoverPanic(r)
-		}
-	}()
-
 	log.Printf("Received: %s (priority: %d)", m.Body, m.Priority)
 
 	// Create new worker for the actual work and perform it
@@ -55,25 +46,6 @@ func (m *messageWorker) Work(ctx context.Context) (err error) {
 
 	// Everything went fine, ack the message
 	m.Ack(false)
-
-	return
-}
-
-func (m *messageWorker) recoverPanic(r interface{}) (err error) {
-	log.Printf("Panic in: %s", m.Body)
-
-	// Permanently remove message from original queue
-	m.Reject(false)
-
-	// find out exactly what the error was and set err
-	switch x := r.(type) {
-	case string:
-		err = errors.New(x)
-	case error:
-		err = x
-	default:
-		err = fmt.Errorf("Unassertable panic error: %v", r)
-	}
 
 	return
 }
