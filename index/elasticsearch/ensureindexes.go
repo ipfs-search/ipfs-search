95cb0a7804e49553ca4cd4ee662698ea9f653059
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-06-24 12:11:47 +0100

    Cleanup EnsureIndexes.

--- index/elasticsearch/ensureindexes.go
+++ index/elasticsearch/ensureindexes.go
@@ -7,49 +7,27 @@ import (
 	"log"
 )
 
-func getElastic(url string) (*elastic.Client, error) {
-	el, err := elastic.NewClient(elastic.SetSniff(false), elastic.SetURL(url))
-	if err != nil {
-		return nil, err
-	}
-
-	log.Printf("Connected to ElasticSearch")
-
-	return el, nil
-}
-
-func getIndex(ctx context.Context, es *elastic.Client, config *index.Config) (index.Index, error) {
-	if config == nil {
-		panic("configuration for index nil")
-	}
-
-	i := &Index{
-		Client: es,
-		Config: config,
-	}
-
-	// Create index if it doesn't already exists, update if it is different (last parameter, true).
-	if err := index.EnsureUpdated(ctx, i, config); err != nil {
-		return nil, err
-	}
-
-	return i, nil
-}
-
 func EnsureIndexes(ctx context.Context, esURL string, configs map[string]*index.Config) (indexes map[string]index.Index, err error) {
-	es, err := getElastic(esURL)
-
+	es, err := elastic.NewClient(elastic.SetSniff(false), elastic.SetURL(esURL))
 	if err != nil {
 		return
 	}
 
+	log.Printf("Connected to ElasticSearch")
+
 	indexes = make(map[string]index.Index, len(configs))
 
 	for n, c := range configs {
-		indexes[n], err = getIndex(ctx, es, c)
-		if err != nil {
+		i := &Index{
+			Client: es,
+			Config: c,
+		}
+
+		if err = index.EnsureUpdated(ctx, i, c); err != nil {
 			return
 		}
+
+		indexes[n] = i
 	}
 
 	return
