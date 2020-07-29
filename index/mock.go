e5831157711f3d6ea538965fcdf1c7d8c0d16253
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-07-29 11:59:41 +0100

    Remove index management (over manual management), update index documentation for ES7.
    * Add term vectors to allow large content fields.
    * Add MinHash/fingerprint to content.
    * Default query fields, prevents “Field expansion matches too many fields”.

--- index/mock.go
+++ index/mock.go
@@ -23,23 +23,3 @@ func (m *mockIndex) Get(ctx context.Context, id string, dst interface{}, fields 
 	args := m.Called(ctx, id, dst, fields)
 	return args.Bool(0), args.Error(1)
 }
-
-func (m *mockIndex) Exists(ctx context.Context) (bool, error) {
-	args := m.Called(ctx)
-	return args.Bool(0), args.Error(1)
-}
-
-func (m *mockIndex) Create(ctx context.Context) error {
-	args := m.Called(ctx)
-	return args.Error(0)
-}
-
-func (m *mockIndex) ConfigUpToDate(ctx context.Context) (bool, error) {
-	args := m.Called(ctx)
-	return args.Bool(0), args.Error(1)
-}
-
-func (m *mockIndex) ConfigUpdate(ctx context.Context) error {
-	args := m.Called(ctx)
-	return args.Error(0)
-}
