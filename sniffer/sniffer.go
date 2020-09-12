e7066037a5ada4b16f228e02ca120d0cdb0381a2
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-09-12 19:42:14 +0100

    Simply API further

--- sniffer/sniffer.go
+++ sniffer/sniffer.go
@@ -14,22 +14,26 @@ type Sniffer struct {
 	es eventsource.EventSource
 }
 
-func New(ds datastore.Batching) (*Sniffer, datastore.Batching, error) {
+func New(ds datastore.Batching) (*Sniffer, error) {
 	bus := eventbus.NewBus()
 
 	es, err := eventsource.New(bus, ds)
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to get eventsource: %w", err)
+		return nil, fmt.Errorf("failed to get eventsource: %w", err)
 	}
 
 	s := Sniffer{
 		es: es,
 	}
 
-	return &s, es.Batching(), nil
+	return &s, nil
 }
 
-func (s Sniffer) Sniff(ctx context.Context) error {
+func (s *Sniffer) Batching() datastore.Batching {
+	return s.es.Batching()
+}
+
+func (s *Sniffer) Sniff(ctx context.Context) error {
 	eventHandler := func(ctx context.Context, e eventsource.EvtProviderPut) error {
 		log.Printf("%+v\n", e)
 		return nil
