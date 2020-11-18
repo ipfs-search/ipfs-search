1feba0d00a66a13283290010b98b78cd01fa6dc1
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-11-18 15:03:50 +0000

    Added index types properly specifying data models.

---
+++ index/types/document.go
@@ -0,0 +1,19 @@
+package types
+
+import (
+	"time"
+)
+
+type Reference struct {
+	ParentHash string `json:"parent_hash"`
+	Name       string `json:"name"`
+}
+
+type References []Reference
+
+type Document struct {
+	FirstSeen  time.Time  `json:"first-seen"`
+	LastSeen   time.Time  `json:"last-seen"`
+	References References `json:"references"`
+	Size       int        `json:"size"`
+}
