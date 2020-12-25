f112a9c5c79f163a032946498cc59e5402341735
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-12-25 13:51:29 +0000

    Note potentially problematic URL for Extractor.

---
+++ extractor/tika/extractor_test.go
@@ -0,0 +1,4 @@
+package tika
+
+// http://ipfs-tika:8081/ipfs/QmehSxmTPRCr85Xjgzjut6uWQihoTfqg9VVihJ892bmZCp/Killing_Yourself_to_Live:_85%_of_a_True_Story.html
+// panic: creating request: parse http://ipfs-tika:8081/ipfs/QmehSxmTPRCr85Xjgzjut6uWQihoTfqg9VVihJ892bmZCp/Killing_Yourself_to_Live:_85%_of_a_True_Story.html: invalid URL escape "%_o"
