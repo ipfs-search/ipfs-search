425d8cdbf3bc42ffbad8701e429ea713c5a57ae7
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-12-25 13:51:11 +0000

    Disable dead letter exchange.

--- queue/amqp/channel.go
+++ queue/amqp/channel.go
@@ -30,27 +30,8 @@ func (c *Channel) Queue(ctx context.Context, name string) (*Queue, error) {
 		false, // exclusive
 		false, // no-wait
 		amqp.Table{
-			"x-max-priority":            9,                   // Enable all 9 priorities
-			"x-message-ttl":             1000 * 60 * 60 * 24, // Expire messages after 24 hours
-			"x-dead-letter-exchange":    "",                  // Anything failing or expiring goes here
-			"x-dead-letter-routing-key": deadQueue,
-			"x-max-length":              100 * 1000, // Max 100.000 messages; prevent resource exhaustion in queue.
-		},
-	)
-	if err != nil {
-		span.RecordError(ctx, err, trace.WithErrorStatus(codes.Error))
-		return nil, err
-	}
-
-	_, err = c.ch.QueueDeclare(
-		deadQueue, // name
-		true,      // durable
-		false,     // delete when unused
-		false,     // exclusive
-		false,     // no-wait
-		amqp.Table{
-			"x-message-ttl": 1000 * 60 * 60 * 24, // Expire messages after 24 hours,
-			"x-max-length":  100 * 1000,          // Max 100.000 messages; prevent resource exhaustion in queue.
+			"x-max-priority": 9,                   // Enable all 9 priorities
+			"x-message-ttl":  1000 * 60 * 60 * 24, // Expire messages after 24 hours
 		},
 	)
 	if err != nil {
