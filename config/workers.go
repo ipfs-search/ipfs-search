02f5e7081c2141fec1bcdeabf582ed99e67f4ede
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-12-30 09:15:40 +0000

    Worker config improvements.
    - Worker count configurable through env.
    - Disable (temporarily) unused StartupDelay.

--- config/workers.go
+++ config/workers.go
@@ -1,20 +1,20 @@
 package config
 
-import (
-	"time"
-)
+// import (
+// 	"time"
+// )
 
 type Workers struct {
 	// RetryWait       time.Duration     `yaml:"retry_wait"` // Time to wait between failed http requests.
-	StartupDelay     time.Duration `yaml:"startup_delay"`
-	HashWorkers      uint          `yaml:"hash_workers"`
-	FileWorkers      uint          `yaml:"file_workers"`
-	DirectoryWorkers uint          `yaml:"directory_workers"`
+	// StartupDelay     time.Duration `yaml:"startup_delay"`
+	HashWorkers      uint `yaml:"hash_workers" env:"HASH_WORKERS"`
+	FileWorkers      uint `yaml:"file_workers" env:"FILE_WORKERS"`
+	DirectoryWorkers uint `yaml:"directory_workers" env:"DIRECTORY_WORKERS"`
 }
 
 func WorkersDefaults() Workers {
 	return Workers{
-		StartupDelay:     time.Duration(100 * time.Millisecond),
+		// StartupDelay:     time.Duration(100 * time.Millisecond),
 		HashWorkers:      70,
 		FileWorkers:      120,
 		DirectoryWorkers: 70,
