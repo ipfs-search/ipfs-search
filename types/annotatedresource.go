6c05710572c2044baaf83f32a87a80b304432684
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-02-05 16:24:48 +0000

    Stringer interface on AnnotatedResource.

--- types/annotatedresource.go
+++ types/annotatedresource.go
@@ -1,8 +1,21 @@
 package types
 
+import (
+	"fmt"
+)
+
 // AnnotatedResource annotates a referenced Resource with additional information.
 type AnnotatedResource struct {
 	*Resource
 	Reference `json:",omitempty"`
 	Stat      `json:",omitempty"`
+}
+
+// String returns the first reference or the URI.
+func (r *AnnotatedResource) String() string {
+	if r.Reference.Name != "" {
+		return fmt.Sprintf("%s (%s)", r.Reference.Name, r.URI())
+	}
+
+	return r.URI()
 }
