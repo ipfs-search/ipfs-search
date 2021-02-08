20e02bbd7cf4bfaaa49ff9ba2f0d8d95384a3431
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-02-08 22:43:10 +0000

    Cleanup IsValidResourceErr.

--- components/protocol/ipfs/isinvalidresourceerr.go
+++ components/protocol/ipfs/isinvalidresourceerr.go
@@ -1,13 +1,19 @@
 package ipfs
 
 import (
+	"context"
+	"errors"
 	ipfs "github.com/ipfs/go-ipfs-api"
 	"log"
 )
 
 // isInvalidResourceErr determines whether an error returned by protocol methods represents invalid content.
 func isInvalidResourceErr(err error) bool {
-	// *ipfs.ERror
+	if errors.Is(err, context.DeadlineExceeded) {
+		// Timeouts are explicitly not protocol errors
+		return false
+	}
+
 	ipfsErr, ok := err.(*ipfs.Error)
 
 	if !ok {
@@ -20,6 +26,7 @@ func isInvalidResourceErr(err error) bool {
 	switch ipfsErr.Message {
 	case "proto: required field \"Type\" not set", // Example: QmYAqhbqNDpU7X9VW6FV5imtngQ3oBRY35zuDXduuZnyA8
 		"proto: unixfs_pb.Data: illegal tag 0 (wire type 0)", // Example: QmQkaTUmqcdGAXKaFXpe8t8yaEDGHe7xGQJHcfihrzAFTj
+		"proto: unixfs_pb.Data: illegal tag 0 (wire type 2)",
 		"unexpected EOF",                 // Example: QmdtMPULYK2xBVt2stYdAdxmuQukbJNFEgsdB5KV3jvsBq
 		"unrecognized object type: 144",  // Example: z43AaGEvwdfzjrCZ3Sq7DKxdDHrwoaPQDtqF4jfdkNEVTiqGVFW
 		"not unixfs node (proto or raw)": // Example: z8mWaJHXieAVxxLagBpdaNWFEBKVWmMiE
