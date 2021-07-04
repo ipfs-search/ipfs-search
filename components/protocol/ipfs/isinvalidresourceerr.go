4dc8da54a6f4e7c2ee31126548c47d56ccfd107a
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-07-04 21:55:25 +0100

    String match based/generic invalids.

--- components/protocol/ipfs/isinvalidresourceerr.go
+++ components/protocol/ipfs/isinvalidresourceerr.go
@@ -5,7 +5,18 @@ import (
 	"errors"
 	ipfs "github.com/ipfs/go-ipfs-api"
 	"log"
+	"strings"
 )
+
+var invalidErrorPrefixes = [...]string{
+	"proto: required field",                // Example: QmYAqhbqNDpU7X9VW6FV5imtngQ3oBRY35zuDXduuZnyA8
+	"proto: unixfs_pb.Data: illegal tag 0", // Example: QmQkaTUmqcdGAXKaFXpe8t8yaEDGHe7xGQJHcfihrzAFTj
+	"unexpected EOF",                       // Example: QmdtMPULYK2xBVt2stYdAdxmuQukbJNFEgsdB5KV3jvsBq
+	"unrecognized object type",             // Example: z43AaGEvwdfzjrCZ3Sq7DKxdDHrwoaPQDtqF4jfdkNEVTiqGVFW
+	"not unixfs node",                      // Example: z8mWaJHXieAVxxLagBpdaNWFEBKVWmMiE
+	"proto: can't skip unknown wire type",
+	"failed to decode Protocol Buffers",
+}
 
 // isInvalidResourceErr determines whether an error returned by protocol methods represents invalid content.
 func isInvalidResourceErr(err error) bool {
@@ -21,20 +32,14 @@ func isInvalidResourceErr(err error) bool {
 		return false
 	}
 
-	log.Printf("*ipfs.Error: %v", ipfsErr.Message)
-
-	switch ipfsErr.Message {
-	case "proto: required field \"Type\" not set", // Example: QmYAqhbqNDpU7X9VW6FV5imtngQ3oBRY35zuDXduuZnyA8
-		"proto: unixfs_pb.Data: illegal tag 0 (wire type 0)", // Example: QmQkaTUmqcdGAXKaFXpe8t8yaEDGHe7xGQJHcfihrzAFTj
-		"proto: unixfs_pb.Data: illegal tag 0 (wire type 2)",
-		"unexpected EOF",                 // Example: QmdtMPULYK2xBVt2stYdAdxmuQukbJNFEgsdB5KV3jvsBq
-		"unrecognized object type: 144",  // Example: z43AaGEvwdfzjrCZ3Sq7DKxdDHrwoaPQDtqF4jfdkNEVTiqGVFW
-		"not unixfs node (proto or raw)", // Example: z8mWaJHXieAVxxLagBpdaNWFEBKVWmMiE
-		"failed to decode Protocol Buffers: incorrectly formatted merkledag node: unmarshal failed. proto: illegal wireType 6", // Example: Qmab9sm49cYmgYfVM812qnAx34VkHRpoAJBLttC41YK3fg
-		"proto: can't skip unknown wire type 6", // Example: QmTPFCJ6oSgevyifNhoK7pL7cznezgquputYn4VVVkYxYo
-		"failed to decode Protocol Buffers: incorrectly formatted merkledag node: unmarshal failed. unexpected EOF": // Example: QmahhA8bbJ2JkdkP8PuCY9FUUbckEsDE44Zwt9yviW8R7G
-		return true
+	for _, p := range invalidErrorPrefixes {
+		if strings.HasPrefix(ipfsErr.Message, p) {
+			// Known invalid.
+			return true
+		}
 	}
+
+	log.Printf("Unexpected *ipfs.Error: %v", ipfsErr.Message)
 
 	return false
 }
