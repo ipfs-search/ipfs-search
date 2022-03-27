7f63712acd700488bbdeb708c7a675e4a64425a9
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-03-27 21:11:37 +0100

    Cleanup 4.

--- components/index/elasticsearch/bulk_getter.go
+++ components/index/elasticsearch/bulk_getter.go
@@ -43,6 +43,132 @@ type reqresp struct {
 	dst  interface{}
 }
 
+type bulkRequest map[string]reqresp
+
+func (r bulkRequest) bulkResponse(found bool, err error) {
+	for _, rr := range r {
+		rr.resp <- GetResponse{found, err}
+	}
+}
+
+func (r bulkRequest) getSearchRequest() *opensearchapi.SearchRequest {
+	// Populate cids and get original fields value
+	var (
+		fields, index []string
+		i             int
+	)
+
+	ids := make([]string, len(r))
+
+	for id, rr := range r {
+		ids[i] = id
+
+		if i == 0 {
+			fields = rr.req.Fields
+			index = []string{rr.req.Index}
+		}
+
+		i++
+	}
+
+	body := getReqBody(ids)
+
+	req := opensearchapi.SearchRequest{
+		Index:          index,
+		SourceIncludes: fields,
+		Body:           strings.NewReader(body),
+		// Preference:     "_local",
+	}
+
+	return &req
+}
+
+func getReqBody(ids []string) string {
+	return `
+	{
+		"query": {
+			"id": {
+				"values": [` + strings.Join(ids, ", ") + `]
+			}
+		}
+	}
+	`
+}
+
+func (r bulkRequest) processResponse(res *opensearchapi.Response) error {
+	switch res.StatusCode {
+	case 200:
+		// Found
+
+		type hit struct {
+			Index      string          `json:"_index"`
+			DocumentID string          `json:"_id`
+			Source     json.RawMessage `json:"_source"`
+		}
+
+		response := struct {
+			Hits struct {
+				Hits []hit `json:"hits"`
+			} `json:"hits"`
+		}{}
+
+		if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+			err = fmt.Errorf("error decoding body: %w", err)
+			// span.RecordError(ctx, err, trace.WithErrorStatus(codes.Error))
+			r.bulkResponse(false, err)
+			return err
+		}
+
+		for _, hit := range response.Hits.Hits {
+			// Write destination data and response for hits
+			rr := r[hit.DocumentID]
+
+			// Decode source into destination
+			err := json.Unmarshal(hit.Source, rr.dst)
+			if err != nil {
+				err = fmt.Errorf("error decoding source: %w", err)
+				// span.RecordError(ctx, err, trace.WithErrorStatus(codes.Error))
+				r.bulkResponse(false, err)
+				return err
+			}
+			rr.resp <- GetResponse{true, nil}
+
+			// Remove from map to separate found from not found
+			delete(r, hit.DocumentID)
+		}
+
+	case 404:
+		// None found, effectively mark all documents as not found.
+
+	default:
+		// return err
+		panic("unexpected status from search")
+	}
+
+	return nil
+}
+
+func (r bulkRequest) performBulkRequest(ctx context.Context, client *opensearch.Client) error {
+	// Perform search request
+	res, err := r.getSearchRequest().Do(ctx, client)
+	if err != nil {
+		r.bulkResponse(false, err)
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if err = r.processResponse(res); err != nil {
+		return err
+	}
+
+	r.bulkResponse(false, nil)
+
+	return nil
+}
+
+type batch map[string]map[string]bulkRequest
+
 // BatchingGetter allows batching/bulk gets.
 type BatchingGetter struct {
 	config BatchingGetterConfig
@@ -80,8 +206,6 @@ func getFieldsKey(fields []string) string {
 	return strings.Join(fields, "")
 }
 
-type batch map[string]map[string]map[string]reqresp
-
 func (bg *BatchingGetter) populateBatch(ctx context.Context, queue <-chan reqresp) (batch, error) {
 	var b batch
 
@@ -100,84 +224,14 @@ func (bg *BatchingGetter) populateBatch(ctx context.Context, queue <-chan reqres
 	return b, nil
 }
 
-func (bg *BatchingGetter) performRequest(ctx context.Context, reqMap map[string]reqresp) error {
-	// Perform search request
-	req := getSearchRequest(reqMap)
-	res, err := req.Do(ctx, bg.config.Client)
-
-	if err != nil {
-		// Propagate error responses
-		for _, rr := range reqMap {
-			rr.resp <- GetResponse{false, err}
-		}
-		return err
-	}
-	defer res.Body.Close()
-
-	switch res.StatusCode {
-	case 200:
-		// Found
-
-		type hit struct {
-			Index      string          `json:"_index"`
-			DocumentID string          `json:"_id`
-			Source     json.RawMessage `json:"_source"`
-		}
-
-		response := struct {
-			Hits struct {
-				Hits []hit `json:"hits"`
-			} `json:"hits"`
-		}{}
-
-		if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
-			err = fmt.Errorf("error decoding body: %w", err)
-			// span.RecordError(ctx, err, trace.WithErrorStatus(codes.Error))
-			// Propagate error responses
-			for _, rr := range reqMap {
-				rr.resp <- GetResponse{false, err}
-			}
-			return err
-		}
-
-		for _, hit := range response.Hits.Hits {
-			// Write destination data and response for hits
-			rr := reqMap[hit.DocumentID]
-
-			// Decode source into destination
-			err = json.Unmarshal(hit.Source, rr.dst)
-			if err != nil {
-				err = fmt.Errorf("error decoding source: %w", err)
-				// span.RecordError(ctx, err, trace.WithErrorStatus(codes.Error))
-				rr.resp <- GetResponse{false, err}
-				return err
-			}
-			rr.resp <- GetResponse{true, nil}
-
-			// Remove from map to separate found from not found
-			delete(reqMap, hit.DocumentID)
-		}
-
-	case 404:
-		// None found, effectively mark all documents as not found.
-
-	default:
-		// return err
-		panic("unexpected status from search")
-	}
-
-	// Mark remaining documents as not found
-	for _, rr := range reqMap {
-		rr.resp <- GetResponse{false, nil}
-	}
-
-	return nil
-}
-
 func (bg *BatchingGetter) performBatch(ctx context.Context, b batch) error {
 	for _, indexes := range b {
-		for _, requestMap := range indexes {
-			bg.performRequest(ctx, requestMap)
+		for _, r := range indexes {
+			err := r.performBulkRequest(ctx, bg.config.Client)
+			if err != nil {
+				// Note: this will terminate batch on errors in any requests.
+				return err
+			}
 		}
 	}
 
@@ -185,6 +239,7 @@ func (bg *BatchingGetter) performBatch(ctx context.Context, b batch) error {
 }
 
 // Work starts a worker to process bulk gets.
+// Note: the worker will terminate on any errors.
 func (bg *BatchingGetter) Work(ctx context.Context) error {
 	b, err := bg.populateBatch(ctx, bg.queue)
 
@@ -193,48 +248,4 @@ func (bg *BatchingGetter) Work(ctx context.Context) error {
 	}
 
 	return bg.performBatch(ctx, b)
-}
-
-func getSearchRequest(reqMap map[string]reqresp) *opensearchapi.SearchRequest {
-	// Populate cids and get original fields value
-	var (
-		fields, index []string
-		i             int
-	)
-
-	ids := make([]string, len(reqMap))
-
-	for id, rr := range reqMap {
-		ids[i] = id
-
-		if i == 0 {
-			fields = rr.req.Fields
-			index = []string{rr.req.Index}
-		}
-
-		i++
-	}
-
-	body := getReqBody(ids)
-
-	req := opensearchapi.SearchRequest{
-		Index:          index,
-		SourceIncludes: fields,
-		Body:           strings.NewReader(body),
-		// Preference:     "_local",
-	}
-
-	return &req
-}
-
-func getReqBody(ids []string) string {
-	return `
-	{
-		"query": {
-			"id": {
-				"values": [` + strings.Join(ids, ", ") + `]
-			}
-		}
-	}
-	`
 }
