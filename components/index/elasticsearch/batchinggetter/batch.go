5a9398589e25abb7089db7bded92617a1ed79b48
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-03-28 10:23:22 +0100

    Simplify; reduce iteration depth.

--- components/index/elasticsearch/batchinggetter/batch.go
+++ components/index/elasticsearch/batchinggetter/batch.go
@@ -7,24 +7,21 @@ import (
 	"github.com/opensearch-project/opensearch-go"
 )
 
-type batch map[string]map[string]bulkRequest
+type batch map[string]bulkRequest
 
-func getFieldsKey(fields []string) string {
-	return strings.Join(fields, "")
+func getKey(rr reqresp) string {
+	return strings.Join(rr.req.Fields, "") + rr.req.Index
 }
 
 func (b batch) add(rr reqresp) {
-	b[getFieldsKey(rr.req.Fields)][rr.req.Index][rr.req.DocumentID] = rr
+	b[getKey(rr)][rr.req.DocumentID] = rr
 }
 
 func (b batch) execute(ctx context.Context, client *opensearch.Client) error {
-	for _, indexes := range b {
-		for _, r := range indexes {
-			err := r.performBulkRequest(ctx, client)
-			if err != nil {
-				// Note: this will terminate batch on first error in request.
-				return err
-			}
+	for _, br := range b {
+		if err := br.performBulkRequest(ctx, client); err != nil {
+			// Note: this will terminate batch on first error in request.
+			return err
 		}
 	}
 
