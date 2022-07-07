4db00d406ed7df319f371519239fba0abfe29495
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-07-07 13:48:49 +0100

    Tests basically pass.

--- components/index/elasticsearch/index_test.go
+++ components/index/elasticsearch/index_test.go
@@ -105,10 +105,10 @@ func (s *IndexTestSuite) TestIndex() {
 	idx := New(s.mockClient, &Config{Name: "test"})
 
 	// Note whitespace here! This is NDJSON
-	request := []byte(`{"create":{"_id":"objId","_index":"test"}}
+	request := []byte(`{"create":{"_index":"test","_id":"objId"}}
 {"field1":"hoi","field2":4}
-
 `)
+
 	response := []byte(`{
 	   "took": 30,
 	   "errors": false,
@@ -165,9 +165,8 @@ func (s *IndexTestSuite) TestUpdate() {
 	idx := New(s.mockClient, &Config{Name: "test"})
 
 	// Note whitespace here! This is NDJSON
-	request := []byte(`{"update":{"_id":"objId","_index":"test"}}
+	request := []byte(`{"update":{"_index":"test","_id":"objId"}}
 {"doc":{"field1":"hoi","field2":4}}
-
 `)
 	response := []byte(`{
 	   "took": 30,
@@ -225,9 +224,8 @@ func (s *IndexTestSuite) TestUpdateOmitEmpty() {
 	idx := New(s.mockClient, &Config{Name: "test"})
 
 	// Note whitespace here! This is NDJSON
-	request := []byte(`{"update":{"_id":"objId","_index":"test"}}
+	request := []byte(`{"update":{"_index":"test","_id":"objId"}}
 {"doc":{}}
-
 `)
 	response := []byte(`{
 	   "took": 30,
@@ -285,7 +283,7 @@ func (s *IndexTestSuite) TestDelete() {
 	idx := New(s.mockClient, &Config{Name: "test"})
 
 	// Note whitespace here! This is NDJSON
-	request := []byte(`{"delete":{"_id":"objId","_index":"test"}}
+	request := []byte(`{"delete":{"_index":"test","_id":"objId"}}
 `)
 	response := []byte(`{
 	   "took": 30,
